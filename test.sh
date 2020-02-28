22ff91529615b8ea053dabdc846f324af3144120
Morgan Guimard <morgan.guimard@docdoku.com>
2020-02-28 12:07:47 +0100

    no chmod needed

--- test.sh
+++ test.sh
@@ -3,7 +3,6 @@
 docker build --build-arg USERID=$(id -u) --build-arg GROUPID=$(id -g) -t mguimard/file-sharing-in-docker:latest .
 
 echo "to be removed" > volume/remove.txt
-chmod g+w volume/remove.txt
 
 docker run --rm -v $(pwd)/volume:/data mguimard/file-sharing-in-docker:latest sh -c "ls -al /data && rm /data/remove.txt && echo Hello > /data/file.txt"
 rm -f volume/file.txt
